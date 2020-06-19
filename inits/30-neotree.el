8f7906b9e65d1f75481e5445a538168e4fff5241
NAOKI KOBAYASHI <id.262.1205@gmail.com>
2020-06-19 07:46:39 +0000

    fix type

--- inits/30-neotree.el
+++ inits/30-neotree.el
@@ -2,7 +2,7 @@
 (require 'neotree)
 ;; 隠しファイルをデフォルトで表示
 (setq neo-show-hidden-files t)
-;; f8f でneotreeをon/off
+;; F8 でneotreeをon/off
 (global-set-key [f8] 'neotree-toggle)
 (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
 
